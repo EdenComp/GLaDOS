3c9b6c44fe84472e459b49af0259ff2fa0e15aee
Croos3r <dorian.moy@epitech.eu>
2023-11-30 02:22:23 +0100

    feat: basic s-expression and abstract syntax tree

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,6 @@
-{-
--- EPITECH PROJECT, 2023
--- GLaDOS
--- File description:
--- Lib
--}
-
-module Lib
-    ( glados
-    ) where
+module Lib (
+  glados,
+) where
 
 glados :: IO ()
-glados = putStrLn "GLaDOS"
+glados = putStrLn "Hello, World"
