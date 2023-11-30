0e0e203428de4e8f872e34d06870705aa3ae2929
Croos3r <dorian.moy@epitech.eu>
2023-11-30 02:22:57 +0100

    feat: basic s-expression and abstract syntax tree

---
+++ src/SExpr.hs
@@ -0,0 +1,25 @@
+module SExpr (
+    SymbolicExpression (..),
+    getSymbol,
+    printTree,
+) where
+
+import Data.List (intercalate)
+
+data SymbolicExpression
+    = Integer Int
+    | Symbol String
+    | List [SymbolicExpression]
+
+instance Show SymbolicExpression where
+    show (Integer num) = "a Number " ++ show num
+    show (Symbol sym) = "a Symbol '" ++ sym ++ "'"
+    show (List []) = "an empty List"
+    show (List list) = "a List containing " ++ intercalate ", followed by " (map show list)
+
+getSymbol :: SymbolicExpression -> Maybe String
+getSymbol (Symbol sym) = Just sym
+getSymbol _ = Nothing
+
+printTree :: SymbolicExpression -> Maybe String
+printTree expr = Just (show expr)
