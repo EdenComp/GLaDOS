812936ca77b80143c6ee64e9671d7c114946e70b
Croos3r <dorian.moy@epitech.eu>
2023-11-30 02:29:48 +0100

    feat(ast-sexpr): added float type

--- src/SExpr.hs
+++ src/SExpr.hs
@@ -8,14 +8,16 @@ import Data.List (intercalate)
 
 data SymbolicExpression
     = Integer Int
+    | Float Float
     | Symbol String
     | List [SymbolicExpression]
 
 instance Show SymbolicExpression where
-    show (Integer num) = "a Number " ++ show num
+    show (Integer value) = "an Integer " ++ show value
     show (Symbol sym) = "a Symbol '" ++ sym ++ "'"
     show (List []) = "an empty List"
     show (List list) = "a List containing " ++ intercalate ", followed by " (map show list)
+    show (Float value) = "a Float " ++ show value
 
 getSymbol :: SymbolicExpression -> Maybe String
 getSymbol (Symbol sym) = Just sym
