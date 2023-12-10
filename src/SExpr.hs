71e031842907451d2cad0034d6f15b94ee063ecf
Croos3r <dorian.moy@epitech.eu>
2023-12-10 19:14:20 +0100

    feat(ast-evaluation): 404 integer not found

--- src/SExpr.hs
+++ src/SExpr.hs
@@ -7,17 +7,15 @@ module SExpr (
 import Data.List (intercalate)
 
 data SymbolicExpression
-    = Integer Int
-    | Float Float
+    = Number Float
     | Symbol String
     | List [SymbolicExpression]
 
 instance Show SymbolicExpression where
-    show (Integer value) = "an Integer " ++ show value
+    show (Number val) = "a Number " ++ show val
     show (Symbol sym) = "a Symbol '" ++ sym ++ "'"
     show (List []) = "an empty List"
     show (List list) = "a List containing " ++ intercalate ", followed by " (map show list)
-    show (Float value) = "a Float " ++ show value
 
 getSymbol :: SymbolicExpression -> Maybe String
 getSymbol (Symbol sym) = Just sym
