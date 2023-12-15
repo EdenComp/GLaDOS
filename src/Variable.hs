894ef3bfa7cae350115516186e0b622577966544
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-12-15 21:07:57 +0100

    fix: Linting with merge

--- src/Variable.hs
+++ src/Variable.hs
@@ -5,11 +5,11 @@ import Types (AstNode, Variable (..))
 
 getVariableValue :: [Char] -> [Variable] -> Maybe AstNode
 getVariableValue searchIdentifier (x : xs)
-  | identifier x == searchIdentifier = Just $ value x
-  | otherwise = getVariableValue searchIdentifier xs
+    | identifier x == searchIdentifier = Just $ value x
+    | otherwise = getVariableValue searchIdentifier xs
 getVariableValue _ [] = Nothing
 
 addVariable :: [Char] -> AstNode -> [Variable] -> Maybe [Variable]
 addVariable iden varValue variables
-  | isNothing $ getVariableValue iden variables = Just (variables ++ [Variable{identifier = iden, value = varValue}])
-  | otherwise = Nothing
+    | isNothing $ getVariableValue iden variables = Just (variables ++ [Variable{identifier = iden, value = varValue}])
+    | otherwise = Nothing
