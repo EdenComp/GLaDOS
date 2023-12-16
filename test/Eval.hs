05914ee843ba699303ff48d9bfc09e5174c8c6b6
Florian Lauch <florian.lauchh@gmail.com>
2023-12-16 19:20:14 +0100

    fix: updated tests to the codebase

--- test/Eval.hs
+++ test/Eval.hs
@@ -2,8 +2,9 @@ module Eval
     ( testEvaluation
     ) where
 
-import Ast (AstNode (..), evalAst)
+import Ast (evalAst)
 import Test.HUnit (Test(..), assertEqual)
+import Types (AstNode (..))
 
 testEvaluation :: Test
 testEvaluation = TestList [
@@ -12,14 +13,14 @@ testEvaluation = TestList [
 
 testAstEvaluation :: Test
 testAstEvaluation = TestList [
-    TestCase (assertEqual "basic" (evalAst [Ast.Number 1]) (Just [Ast.Number 1])),
-    TestCase (assertEqual "basic operation" (evalAst [Ast.Call "*" [Ast.Number 12, Ast.Number 3]]) (Just [Ast.Number 36])),
-    TestCase (assertEqual "unknown symbol" (evalAst [Ast.Call "*" [Ast.Symbol "var", Ast.Number 3]]) Nothing),
-    TestCase (assertEqual "defined symbol" (evalAst [Ast.Call "define" [Ast.Symbol "a", Ast.Number 10], Ast.Call "+" [Ast.Symbol "a", Ast.Number 3]]) (Just [Ast.Number 13])),
-    TestCase (assertEqual "two defined symbols" (evalAst [Ast.Call "define" [Ast.Symbol "a", Ast.Number 10], Ast.Call "define" [Ast.Symbol "b", Ast.Number 20], Ast.Call "-" [Ast.Symbol "a", Ast.Symbol "b"]]) (Just [Ast.Number (-10)])),
-    TestCase (assertEqual "combined operations" (evalAst [Ast.Call "*" [Ast.Call "/" [Ast.Number 10, Ast.Number 5], Ast.Call "%" [Ast.Number 19, Ast.Number 10]]]) (Just [Ast.Number 18])),
-    TestCase (assertEqual "operation in symbol" (evalAst [Ast.Call "define" [Ast.Symbol "test", Ast.Call "/" [Ast.Number 10, Ast.Number 5]], Ast.Symbol("test")]) (Just [Ast.Number 2])),
-    TestCase (assertEqual "define without operation" (evalAst [Ast.Call "define" [Ast.Symbol "const", Ast.Number 10]]) (Just []))
-    --TestCase (assertEqual "reassignment" (evalAst [Ast.Call "define" [Ast.Symbol "let", Ast.Number 4], Ast.Call "define" [Ast.Symbol "let", Ast.Number 9], Ast.Symbol("let")]) (Just [Ast.Number 9])),
-    --TestCase (assertEqual "reassignment with value" (evalAst [Ast.Call "define" [Ast.Symbol "let", Ast.Number 4], Ast.Call "define" [Ast.Symbol "let", Ast.Call "+" [Ast.Symbol "let", Ast.Number 6]], Ast.Symbol("let")]) (Just [Ast.Number 10]))
+    TestCase (assertEqual "basic" (evalAst [Types.Number 1]) (Just [Types.Number 1])),
+    TestCase (assertEqual "basic operation" (evalAst [Types.Call (Types.Symbol "*") [Types.Number 12, Types.Number 3]]) (Just [Types.Number 36])),
+    TestCase (assertEqual "unknown symbol" (evalAst [Types.Call (Types.Symbol "*") [Types.Symbol "var", Types.Number 3]]) Nothing),
+    TestCase (assertEqual "defined symbol" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Number 3]]) (Just [Types.Number 13])),
+    TestCase (assertEqual "two defined symbols" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Call (Types.Symbol "define") [Types.Symbol "b", Types.Number 20], Types.Call (Types.Symbol "-") [Types.Symbol "a", Types.Symbol "b"]]) (Just [Types.Number (-10)])),
+    TestCase (assertEqual "combined operations" (evalAst [Types.Call (Types.Symbol "*") [Types.Call (Types.Symbol "/") [Types.Number 10, Types.Number 5], Types.Call (Types.Symbol "%") [Types.Number 19, Types.Number 10]]]) (Just [Types.Number 18])),
+    TestCase (assertEqual "operation in symbol" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "test", Types.Call (Types.Symbol "/") [Types.Number 10, Types.Number 5]], Types.Symbol("test")]) (Just [Types.Number 2])),
+    TestCase (assertEqual "define without operation" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "const", Types.Number 10]]) (Just [])),
+    --TestCase (assertEqual "reassignment" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 4], Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 9], Types.Symbol("let")]) (Just [Types.Number 9])),
+    --TestCase (assertEqual "reassignment with value" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 4], Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Call (Types.Symbol "+") [Types.Symbol "let", Types.Number 6]], Types.Symbol("let")]) (Just [Types.Number 10]))
     ]
