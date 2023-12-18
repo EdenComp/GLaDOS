b52113504883dda8618bbe79f256d4c203eeb94c
Florian Lauch <florian.lauchh@gmail.com>
2023-12-18 17:13:09 +0100

    feat: Unit tests on edge cases

--- test/TestEval.hs
+++ test/TestEval.hs
@@ -4,29 +4,69 @@ module TestEval
 
 import Ast (evalAst)
 import Test.HUnit (Test(..), assertEqual)
-import Types (AstNode (..))
+import Types (AstNode (..), Variable(..))
 
 testEvaluation :: Test
 testEvaluation = TestList [
-    testAstEvaluation
+    testOperations,
+    testLambdas,
+    testDeclarations,
+    testReassignments,
+    testMisc
     ]
 
-testAstEvaluation :: Test
-testAstEvaluation = TestList [
+testOperations :: Test
+testOperations = TestList [
     TestCase (assertEqual "basic" (evalAst [Types.Number 1]) (Just [Types.Number 1])),
     TestCase (assertEqual "basic operation" (evalAst [Types.Call (Types.Symbol "*") [Types.Number 12, Types.Number 3]]) (Just [Types.Number 36])),
     TestCase (assertEqual "unknown symbol" (evalAst [Types.Call (Types.Symbol "*") [Types.Symbol "var", Types.Number 3]]) Nothing),
     TestCase (assertEqual "unknown call" (evalAst [Types.Call (Types.Symbol "_") [Types.Number 1, Types.Number 3]]) Nothing),
+    TestCase (assertEqual "wrong parameters" (evalAst [Types.Call (Types.Symbol "*") [Types.Call (Types.Symbol "+") [Types.Boolean True, Types.Number 1], Types.Number 3]]) Nothing),
+    TestCase (assertEqual "void" (evalAst [Types.Void]) (Just []))
+    ]
+
+testDeclarations :: Test
+testDeclarations = TestList [
     TestCase (assertEqual "defined symbol" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Number 3]]) (Just [Types.Number 13])),
+    TestCase (assertEqual "reversed symbol" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Call (Types.Symbol "+") [Types.Number 3, Types.Symbol "a"]]) (Just [Types.Number 13])),
     TestCase (assertEqual "two defined symbols" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Call (Types.Symbol "define") [Types.Symbol "b", Types.Number 20], Types.Call (Types.Symbol "-") [Types.Symbol "a", Types.Symbol "b"]]) (Just [Types.Number (-10)])),
     TestCase (assertEqual "combined operations" (evalAst [Types.Call (Types.Symbol "*") [Types.Call (Types.Symbol "/") [Types.Number 10, Types.Number 5], Types.Call (Types.Symbol "%") [Types.Number 19, Types.Number 10]]]) (Just [Types.Number 18])),
     TestCase (assertEqual "operation in symbol" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "test", Types.Call (Types.Symbol "/") [Types.Number 10, Types.Number 5]], Types.Symbol("test")]) (Just [Types.Number 2])),
     TestCase (assertEqual "define without operation" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "const", Types.Number 10]]) (Just [])),
-    TestCase (assertEqual "void" (evalAst [Types.Void]) (Just [])),
-    TestCase (assertEqual "void with define" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Void]) (Just [])),
+    TestCase (assertEqual "void with define" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 10], Types.Void]) (Just []))
+    ]
+
+testLambdas :: Test
+testLambdas = TestList [
     TestCase (assertEqual "basic lambda" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "add", Types.Lambda ["a", "b"] (Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Symbol "b"])], Types.Call (Types.Symbol "add") [Types.Number 12, Types.Number 3]]) (Just [Types.Number 15])),
+    TestCase (assertEqual "basic function definition" (evalAst [Types.Call (Symbol "define") [Types.Call (Types.Symbol "sum") [Types.Symbol "a", Types.Symbol "b"], Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Symbol "b"]], Types.Call (Types.Symbol "sum") [Types.Number (-1), Types.Number (-2)]]) (Just [Types.Number (-3)])),
     TestCase (assertEqual "wrong lambda call" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "add", Types.Lambda ["a", "b"] (Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Symbol "b"])], Types.Call (Types.Symbol "add") [Types.Number 12, Types.Number 3, Types.Number 1]]) Nothing),
     TestCase (assertEqual "lambda execution" (evalAst [Types.Call (Types.Call (Types.Symbol "lambda") [Types.Lambda ["a", "b"] (Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Symbol "b"])]) [Types.Number 12, Types.Number 3]]) Nothing),
+    TestCase (assertEqual "lambda definition" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "sum", Types.Call (Types.Symbol "lambda") [Types.Call (Types.Symbol "a") [Types.Symbol "b"], Types.Call (Types.Symbol "+") [Types.Symbol "a", Types.Symbol "b"]]], Types.Call (Types.Symbol "sum") [Types.Number 1, Types.Number 2]]) (Just [Types.Number 3]))
+    ]
+
+testReassignments :: Test
+testReassignments = TestList [
     TestCase (assertEqual "reassignment" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 4], Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 9], Types.Symbol("let")]) (Just [Types.Number 9])),
-    TestCase (assertEqual "reassignment with value" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 4], Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Call (Types.Symbol "+") [Types.Symbol "let", Types.Number 6]], Types.Symbol("let")]) (Just [Types.Number 10]))
+    TestCase (assertEqual "reassignment with value" (evalAst [Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Number 4], Types.Call (Types.Symbol "define") [Types.Symbol "let", Types.Call (Types.Symbol "+") [Types.Symbol "let", Types.Number 6]], Types.Symbol("let")]) (Just [Types.Number 10])),
+    TestCase (assertEqual "multiple reassignments" (evalAst [
+      Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 1],
+      Types.Call (Types.Symbol "define") [Types.Symbol "b", Types.Number 15],
+      Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Symbol "b"],
+      Types.Symbol("a")
+    ]) (Just [Types.Number 15])),
+    TestCase (assertEqual "post multiple reassignments" (evalAst [
+      Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Number 1],
+      Types.Call (Types.Symbol "define") [Types.Symbol "b", Types.Number 15],
+      Types.Call (Types.Symbol "define") [Types.Symbol "a", Types.Symbol "b"],
+      Types.Call (Types.Symbol "define") [Types.Symbol "b", Types.Number 17],
+      Types.Symbol("a"),
+      Types.Symbol("b")
+    ]) (Just [Types.Number 15, Types.Number 17]))
+    ]
+
+testMisc :: Test
+testMisc = TestList [
+    TestCase (assertEqual "show variables" (show (Variable "a" (Types.Number 1))) ("Variable {identifier = \"a\", value = Number 1}")),
+    TestCase (assertEqual "show ast node" (show (Types.Number 1)) ("Number 1"))
     ]
--- test/TestSExpr.hs
+++ test/TestSExpr.hs
@@ -9,7 +9,8 @@ import Types (AstNode (..))
 testSExpr :: Test
 testSExpr = TestList [
     testSExprToAST,
-    testManySExprToAST
+    testManySExprToAST,
+    testSExprShow
     ]
 
 testSExprToAST :: Test
@@ -19,11 +20,21 @@ testSExprToAST = TestList [
     TestCase (assertEqual "boolean false" (sexprToAst (SExpr.Symbol "#f")) (Just (Types.Boolean False))),
     TestCase (assertEqual "list" (sexprToAst (SExpr.List [SExpr.Symbol "if", SExpr.Symbol "#t", SExpr.Number 1, SExpr.Number 2])) (Just (Types.Call (Types.Symbol "if") [Types.Boolean True, Types.Number 1, Types.Number 2]))),
     TestCase (assertEqual "empty" (sexprToAst (SExpr.List [])) (Just (Types.Symbol "()"))),
-    TestCase (assertEqual "list with only one element" (sexprToAst (SExpr.List [SExpr.Number 2])) (Just (Types.Call (Types.Number 2) [])))
+    TestCase (assertEqual "list with one element" (sexprToAst (SExpr.List [SExpr.Number 2])) (Just (Types.Call (Types.Number 2) [])))
     ]
 
 testManySExprToAST :: Test
 testManySExprToAST = TestList [
     TestCase (assertEqual "basic" (sexprsToAsts [SExpr.Number 11]) (Just [Types.Number 11])),
     TestCase (assertEqual "function with call" (sexprsToAsts [SExpr.List [SExpr.Symbol "define", SExpr.Symbol "number", SExpr.Number 1], SExpr.Symbol "number"]) (Just [Types.Call (Types.Symbol "define") [Types.Symbol "number", Types.Number 1], Types.Symbol "number"]))
+    ]
+
+testSExprShow :: Test
+testSExprShow = TestList [
+    TestCase (assertEqual "basic" (show (SExpr.Number 10)) "10"),
+    TestCase (assertEqual "boolean true" (show (SExpr.Symbol "#t")) "#t"),
+    TestCase (assertEqual "boolean false" (show (SExpr.Symbol "#f")) "#f"),
+    TestCase (assertEqual "list" (show (SExpr.List [SExpr.Symbol "if", SExpr.Symbol "#t", SExpr.Number 1, SExpr.Number 2])) "(if #t 1 2)"),
+    TestCase (assertEqual "empty" (show (SExpr.List [])) "()"),
+    TestCase (assertEqual "list with one element" (show (SExpr.List [SExpr.Number 2])) "(2)")
     ]
