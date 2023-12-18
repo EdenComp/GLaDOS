0cc37187c9d6f5eb50a60e11e9a048493cdb24b4
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-12-18 21:42:58 +0100

    fix: remove all text in TTY

--- src/GetInput.hs
+++ src/GetInput.hs
@@ -3,25 +3,14 @@ module GetInput (
 )
 where
 
-import System.IO (hFlush, isEOF, stdout)
-
-colorRed :: String
-colorRed = "\x1b[31m"
-
-colorYellow :: String
-colorYellow = "\x1b[33m"
-
-colorReset :: String
-colorReset = "\x1b[0m"
+import System.IO (isEOF)
 
 getUserInput :: (String -> IO ()) -> IO ()
 getUserInput lispInterpreter =
-    putStr (colorRed ++ "DreamBerd4-Interpreter>>> " ++ colorReset)
-        >> hFlush stdout
-        >> isEOF
+    isEOF
         >>= \eof ->
             if eof
-                then putStrLn (colorYellow ++ "Exit DreamBerd4!" ++ colorReset)
+                then putStrLn ("Exit")
                 else
                     getLine
                         >>= \userInput -> lispInterpreter userInput >> getUserInput lispInterpreter
