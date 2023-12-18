6d92678985325cf19db0d85cfc4177a7f09dab00
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-12-18 23:48:51 +0100

    feat: now stdin has memorie

--- src/GetInput.hs
+++ src/GetInput.hs
@@ -4,13 +4,19 @@ module GetInput (
 where
 
 import System.IO (isEOF)
+import System.Exit (exitWith, ExitCode (..))
 
 getUserInput :: (String -> IO ()) -> IO ()
-getUserInput lispInterpreter =
-    isEOF
-        >>= \eof ->
-            if eof
-                then putStrLn "Exit"
-                else
-                    getLine
-                        >>= \userInput -> lispInterpreter userInput >> getUserInput lispInterpreter
+getUserInput lispInterpreter = getUserInput' lispInterpreter ""
+
+getUserInput' :: (String -> IO ()) -> String -> IO ()
+getUserInput' lispInterpreter linesSoFar = do
+    eof <- isEOF
+    if eof
+        then exitWith ExitSuccess
+        else do
+            userInput <- getLine
+            let updatedLines = linesSoFar ++ userInput
+            lispInterpreter updatedLines
+            getUserInput' lispInterpreter updatedLines
+
