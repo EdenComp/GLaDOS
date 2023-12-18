7b39a8fa5c0a3341b86be2d1c60208d0fda89d99
Croos3r <dorian.moy@epitech.eu>
2023-12-18 03:36:08 +0100

    style: indentation

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,18 +1,18 @@
 module Parsing (
-  parse,
-  Parser,
-  parseChar,
-  parseAnyChar,
-  parseOr,
-  parseAnd,
-  parseAndWith,
-  parseMany,
-  parseSome,
-  parseUInt,
-  parseInt,
-  parsePair,
-  parseLisp,
-  parseWhiteSpace,
+    parse,
+    Parser,
+    parseChar,
+    parseAnyChar,
+    parseOr,
+    parseAnd,
+    parseAndWith,
+    parseMany,
+    parseSome,
+    parseUInt,
+    parseInt,
+    parsePair,
+    parseLisp,
+    parseWhiteSpace,
 ) where
 
 import Control.Applicative (Alternative (..))
@@ -22,41 +22,41 @@ import SExpr (SymbolicExpression (..))
 newtype Parser a = Parser {parse :: String -> Maybe (a, String)}
 
 instance Functor Parser where
-  fmap f (Parser p) = Parser $ \input ->
-    case p input of
-      Just (result, rest) -> Just (f result, rest)
-      Nothing -> Nothing
+    fmap f (Parser p) = Parser $ \input ->
+        case p input of
+            Just (result, rest) -> Just (f result, rest)
+            Nothing -> Nothing
 
 instance Applicative Parser where
-  pure x = Parser $ \input -> Just (x, input)
-  (Parser p1) <*> (Parser p2) = Parser $ \input ->
-    case p1 input of
-      Just (f, rest) -> case p2 rest of
-        Just (result, rest') -> Just (f result, rest')
-        Nothing -> Nothing
-      Nothing -> Nothing
+    pure x = Parser $ \input -> Just (x, input)
+    (Parser p1) <*> (Parser p2) = Parser $ \input ->
+        case p1 input of
+            Just (f, rest) -> case p2 rest of
+                Just (result, rest') -> Just (f result, rest')
+                Nothing -> Nothing
+            Nothing -> Nothing
 
 instance Alternative Parser where
-  empty = Parser $ const Nothing
-  (Parser p1) <|> (Parser p2) = Parser $ \input ->
-    case p1 input of
-      Just (result, rest) -> Just (result, rest)
-      Nothing -> p2 input
+    empty = Parser $ const Nothing
+    (Parser p1) <|> (Parser p2) = Parser $ \input ->
+        case p1 input of
+            Just (result, rest) -> Just (result, rest)
+            Nothing -> p2 input
 
 instance Monad Parser where
-  return = pure
-  (Parser p) >>= f = Parser $ \input ->
-    case p input of
-      Just (result, rest) -> parse (f result) rest
-      Nothing -> Nothing
+    return = pure
+    (Parser p) >>= f = Parser $ \input ->
+        case p input of
+            Just (result, rest) -> parse (f result) rest
+            Nothing -> Nothing
 
 parseChar :: Char -> Parser Char
 parseChar c = Parser f
- where
-  f (x : xs)
-    | x == c = Just (c, xs)
-    | otherwise = Nothing
-  f _ = Nothing
+  where
+    f (x : xs)
+        | x == c = Just (c, xs)
+        | otherwise = Nothing
+    f _ = Nothing
 
 parseAnyChar :: String -> Parser Char
 parseAnyChar = foldr ((<|>) . parseChar) empty
@@ -75,30 +75,30 @@ parseAndWith f p1 p2 = f <$> p1 <*> p2
 
 parseMany :: Parser a -> Parser [a]
 parseMany p = Parser $ \input ->
-  case parse (parseSome p) input of
-    Just (results, rest) -> Just (results, rest)
-    Nothing -> Just ([], input)
+    case parse (parseSome p) input of
+        Just (results, rest) -> Just (results, rest)
+        Nothing -> Just ([], input)
 
 parseSome :: Parser a -> Parser [a]
 parseSome p = Parser $ \input ->
-  case parse p input of
-    Just (result, rest) ->
-      case parse (parseMany p) rest of
-        Just (results, rest') -> Just (result : results, rest')
-        Nothing -> Just ([result], rest)
-    Nothing -> Nothing
+    case parse p input of
+        Just (result, rest) ->
+            case parse (parseMany p) rest of
+                Just (results, rest') -> Just (result : results, rest')
+                Nothing -> Just ([result], rest)
+        Nothing -> Nothing
 
 parseUInt :: Parser Int
 parseUInt = Parser f
- where
-  f input = case span isDigit input of
-    ("", _) -> Nothing
-    (numStr, rest) -> Just (read numStr, rest)
+  where
+    f input = case span isDigit input of
+        ("", _) -> Nothing
+        (numStr, rest) -> Just (read numStr, rest)
 
 parseInt :: Parser Int
 parseInt = parseNeg <|> parseUInt
- where
-  parseNeg = parseChar '-' *> (negate <$> parseUInt)
+  where
+    parseNeg = parseChar '-' *> (negate <$> parseUInt)
 
 parsePair :: Parser a -> Parser (a, a)
 parsePair p = parseChar '(' *> parseAndWith (,) p (parseChar ' ' *> p) <* parseChar ')'
