39b8667757e58367b0becf1a4681b76b372ae980
Florian Lauch <florian.lauchh@gmail.com>
2023-12-18 22:34:53 +0100

    feat: Comparison operators

--- src/Function.hs
+++ src/Function.hs
@@ -3,7 +3,7 @@
 module Function (appendParametersToVariables, evalCall, applyBinaryOpOnNodes, getOperatorForBinaryOpSymbol, evalBinaryOperation) where
 
 import Control.Applicative (Alternative ((<|>)))
-import Types (AstNode (..), BuiltinOperator, NodeEvaluator, Variable (..))
+import Types (AstNode (..), BuiltinOperator, BuiltinComparisonOperator, NodeEvaluator, Variable (..))
 import Variable (addVariable)
 
 appendParametersToVariables :: [Variable] -> [String] -> [AstNode] -> Maybe [Variable]
@@ -17,11 +17,15 @@ evalCall vars (Lambda parameters body) args nodeEvaluator = mapM (nodeEvaluator 
 evalCall vars (Symbol "define") args nodeEvaluator = evalDefine vars args nodeEvaluator >>= \newVariables -> Just (Void, newVariables)
 evalCall vars (Call (Symbol "lambda") [Call arg rest, body]) args nodeEvaluator = evalCall vars (Symbol "lambda") [Call arg rest, body] nodeEvaluator >>= \(ast, newVariables) -> evalCall newVariables ast args nodeEvaluator
 evalCall vars (Symbol "if") args nodeEvaluator = evalIf vars args nodeEvaluator
-evalCall vars func args nodeEvaluator = evalBinaryOperation vars func args nodeEvaluator <|> evalDefinedFunction vars func args nodeEvaluator
+evalCall vars func args nodeEvaluator = evalBinaryOperation vars func args nodeEvaluator <|> evalComparisonOperation vars func args nodeEvaluator <|> evalDefinedFunction vars func args nodeEvaluator
 
 evalBinaryOperation :: [Variable] -> AstNode -> [AstNode] -> NodeEvaluator -> Maybe (AstNode, [Variable])
 evalBinaryOperation vars (Symbol op) [l, r] nodeEvaluator = getOperatorForBinaryOpSymbol op >>= \resolvedOp -> applyBinaryOpOnNodes vars l r resolvedOp nodeEvaluator >>= \ast -> Just (ast, vars)
 evalBinaryOperation _ _ _ _ = Nothing
+
+evalComparisonOperation :: [Variable] -> AstNode -> [AstNode] -> NodeEvaluator -> Maybe (AstNode, [Variable])
+evalComparisonOperation vars (Symbol op) [l, r] nodeEvaluator = getOperatorForBinaryCmpSymbol op >>= \resolvedOp -> applyBinaryCmpOnNodes vars l r resolvedOp nodeEvaluator >>= \ast -> Just (ast, vars)
+evalComparisonOperation _ _ _ _ = Nothing
 
 evalDefinedFunction :: [Variable] -> AstNode -> [AstNode] -> NodeEvaluator -> Maybe (AstNode, [Variable])
 evalDefinedFunction vars (Symbol iden) args nodeEvaluator = nodeEvaluator vars (Symbol iden) >>= \(ast, newVariables) -> evalCall newVariables ast args nodeEvaluator
@@ -60,7 +64,18 @@ getOperatorForBinaryOpSymbol "*" = Just (*)
 getOperatorForBinaryOpSymbol "-" = Just (-)
 getOperatorForBinaryOpSymbol "/" = Just div
 getOperatorForBinaryOpSymbol "%" = Just mod
+getOperatorForBinaryOpSymbol "div" = Just div
+getOperatorForBinaryOpSymbol "mod" = Just mod
 getOperatorForBinaryOpSymbol _ = Nothing
+
+getOperatorForBinaryCmpSymbol :: [Char] -> Maybe BuiltinComparisonOperator
+getOperatorForBinaryCmpSymbol "<" = Just (<)
+getOperatorForBinaryCmpSymbol ">" = Just (>)
+getOperatorForBinaryCmpSymbol "<=" = Just (<=)
+getOperatorForBinaryCmpSymbol ">=" = Just (>=)
+getOperatorForBinaryCmpSymbol "=" = Just (==)
+getOperatorForBinaryCmpSymbol "eq?" = Just (==)
+getOperatorForBinaryCmpSymbol _ = Nothing
 
 applyBinaryOpOnNodes :: [Variable] -> AstNode -> AstNode -> BuiltinOperator -> NodeEvaluator -> Maybe AstNode
 applyBinaryOpOnNodes _ (Number l) (Number r) op _ = Just $ Number (op l r)
@@ -69,3 +84,11 @@ applyBinaryOpOnNodes vars (Call name args) r op nodeEvaluator = nodeEvaluator va
 applyBinaryOpOnNodes vars (Symbol sym) r op nodeEvaluator = nodeEvaluator vars (Symbol sym) >>= \(l, newVariables) -> applyBinaryOpOnNodes newVariables l r op nodeEvaluator
 applyBinaryOpOnNodes _ (Boolean _) _ _ _ = Nothing
 applyBinaryOpOnNodes vars l r op nodeEvaluator = applyBinaryOpOnNodes vars r l op nodeEvaluator
+
+applyBinaryCmpOnNodes :: [Variable] -> AstNode -> AstNode -> BuiltinComparisonOperator -> NodeEvaluator -> Maybe AstNode
+applyBinaryCmpOnNodes _ (Number l) (Number r) op _ = Just $ Boolean (op l r)
+applyBinaryCmpOnNodes vars (Symbol l) (Symbol r) op nodeEvaluator = nodeEvaluator vars (Symbol l) >>= \(lResolved, newVariables) -> nodeEvaluator newVariables (Symbol r) >>= \(rResolved, newNewVariables) -> applyBinaryCmpOnNodes newNewVariables lResolved rResolved op nodeEvaluator
+applyBinaryCmpOnNodes vars (Call name args) r op nodeEvaluator = nodeEvaluator vars (Call name args) >>= \(l, newVariables) -> applyBinaryCmpOnNodes newVariables l r op nodeEvaluator
+applyBinaryCmpOnNodes vars (Symbol sym) r op nodeEvaluator = nodeEvaluator vars (Symbol sym) >>= \(l, newVariables) -> applyBinaryCmpOnNodes newVariables l r op nodeEvaluator
+applyBinaryCmpOnNodes _ (Boolean _) _ _ _ = Nothing
+applyBinaryCmpOnNodes vars l r op nodeEvaluator = applyBinaryCmpOnNodes vars r l op nodeEvaluator
