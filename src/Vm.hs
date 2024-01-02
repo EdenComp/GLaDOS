bd848584a98cea66a78f09b52c9a844870138991
Florian <florian.lauch@epitech.eu>
2024-01-02 16:42:05 +0100

    feat: Added function arguments

--- src/Vm.hs
+++ src/Vm.hs
@@ -26,24 +26,29 @@ data Ops
 
 data Insts
     = Push Value
+    | PushArg Int
     | Call Ops
     | JumpIfFalse Int
     | Ret
 
-exec :: [Value] -> [Insts] -> Either String Value
-exec _ [] = Right Void
-exec [] (Ret:_) = Right Void
-exec (value:_) (Ret:_) = Right value
-exec stack (Push val:insts) = exec (val:stack) insts
-exec stack (Call op:insts) = case execCall stack op of
+exec :: [Value] -> [Value] -> [Insts] -> Either String Value
+exec _ _ [] = Right Void
+exec _ [] (Ret:_) = Right Void
+exec _ (value:_) (Ret:_) = Right value
+exec args stack (Push val:insts) = exec args (val:stack) insts
+exec args stack (PushArg idx:insts)
+    | idx >= length args = Left "Argument index out of bounds"
+    | otherwise = exec args ((args !! idx):stack) insts
+exec args stack (Call op:insts) = case execCall stack op of
     Left err -> Left err
-    Right newValues -> exec newValues insts
-exec [] (JumpIfFalse _:_) = Left "Stack is empty"
-exec (Bool x:xs) (JumpIfFalse num:insts) | num < 1 = Left "Invalid number of instructions"
-                                         | num > length insts = Left "Cannot jump this amount of instructions"
-                                         | not x = exec xs (drop num insts)
-                                         | otherwise = exec xs insts
-exec _ (JumpIfFalse _:_) = Left "Wrong data types in stack: JumpIfFalse needs a Bool"
+    Right newValues -> exec args newValues insts
+exec _ [] (JumpIfFalse _:_) = Left "Stack is empty"
+exec args (Bool x:xs) (JumpIfFalse num:insts) 
+    | num < 1 = Left "Invalid number of instructions"
+    | num > length insts = Left "Cannot jump this amount of instructions"
+    | not x = exec args xs (drop num insts)
+    | otherwise = exec args xs insts
+exec _ _ (JumpIfFalse _:_) = Left "Wrong data types in stack: JumpIfFalse needs a Bool"
 
 
 execCall :: [Value] -> Ops -> Either String [Value]
