a462943d8665b204f592fff30a68502945444d33
Florian <florian.lauch@epitech.eu>
2024-01-02 16:28:01 +0100

    feat: Boolean operations and jump if false

--- src/Vm.hs
+++ src/Vm.hs
@@ -16,28 +16,52 @@ data Ops
     | Sub
     | Mul
     | Div
+    | Eq
+    | Neq
+    | Less
+    | LessOrEqual
+    | Greater
+    | GreaterOrEqual
     deriving (Show)
 
 data Insts
     = Push Value
     | Call Ops
+    | JumpIfFalse Int
     | Ret
 
 exec :: [Value] -> [Insts] -> Either String Value
-exec _ [] = Left "No instructions"
+exec _ [] = Right Void
 exec [] (Ret:_) = Right Void
 exec (value:_) (Ret:_) = Right value
-exec stack (Push val:xs) = exec (val:stack) xs
+exec stack (Push val:insts) = exec (val:stack) insts
 exec stack (Call op:insts) = case execCall stack op of
     Left err -> Left err
     Right newValues -> exec newValues insts
+exec [] (JumpIfFalse _:_) = Left "Stack is empty"
+exec (Bool x:xs) (JumpIfFalse num:insts) | num < 1 = Left "Invalid number of instructions"
+                                         | num > length insts = Left "Cannot jump this amount of instructions"
+                                         | not x = exec xs (drop num insts)
+                                         | otherwise = exec xs insts
+exec _ (JumpIfFalse _:_) = Left "Wrong data types in stack: JumpIfFalse needs a Bool"
+
 
 execCall :: [Value] -> Ops -> Either String [Value]
 execCall (Number 0:Number 0:_) Div = Left "Cannot divide by 0"
 execCall (Number l:Number r:xs) op = case op of 
-    Add -> Right (Number (r + l):xs)
-    Sub -> Right (Number (r - l):xs)
-    Mul -> Right (Number (r * l):xs)
-    Div -> Right (Number (div r l):xs)
+    Add -> Right (Number (l + r):xs)
+    Sub -> Right (Number (l - r):xs)
+    Mul -> Right (Number (l * r):xs)
+    Div -> Right (Number (div l r):xs)
+    Eq -> Right (Bool (l == r):xs)
+    Neq -> Right (Bool (l /= r):xs)
+    Less -> Right (Bool (l < r):xs)
+    LessOrEqual -> Right (Bool (l <= r):xs)
+    Greater -> Right (Bool (l > r):xs)
+    GreaterOrEqual -> Right (Bool (l >= r):xs)
     _ -> Left ("Wrong data types in stack: " ++ show op ++ " with 2 numbers")
+execCall (Bool l:Bool r:xs) op = case op of
+    Eq -> Right (Bool (l == r):xs)
+    Neq -> Right (Bool (l /= r):xs)
+    _ -> Left ("Wrong data types in stack: " ++ show op ++ " with 2 booleans")
 execCall _ _ = Left "Unknown operation"
