ef2c85c3a88a007e9700a74b703de45d3803e806
Florian <florian.lauch@epitech.eu>
2024-01-02 18:08:19 +0100

    feat: Started functions handling

--- src/Vm.hs
+++ src/Vm.hs
@@ -1,17 +1,18 @@
 module Vm (
     exec,
+    Call(..),
     Insts(..),
-    Ops(..),
     Value(..)
 ) where
 
 data Value
     = Number Integer
     | Bool Bool
+    | Symbol Call
     | Void
-    deriving (Eq, Show)
+    deriving (Show)
 
-data Ops
+data Call
     = Add
     | Sub
     | Mul
@@ -22,14 +23,16 @@ data Ops
     | LessOrEqual
     | Greater
     | GreaterOrEqual
+    | Function [Char]
     deriving (Show)
 
 data Insts
     = Push Value
     | PushArg Int
-    | Call Ops
+    | Call
     | JumpIfFalse Int
     | Ret
+    deriving (Show)
 
 exec :: [Value] -> [Value] -> [Insts] -> Either String Value
 exec _ _ [] = Right Void
@@ -39,10 +42,10 @@ exec args stack (Push val:insts) = exec args (val:stack) insts
 exec args stack (PushArg idx:insts)
     | idx >= length args = Left "Argument index out of bounds"
     | otherwise = exec args ((args !! idx):stack) insts
-exec args stack (Call op:insts) = case execCall stack op of
+exec args stack (Call:insts) = case execCall stack of
     Left err -> Left err
     Right newValues -> exec args newValues insts
-exec _ [] (JumpIfFalse _:_) = Left "Stack is empty"
+exec _ [] (x:_) = Left ("Stack is empty for a " ++ show x ++ " instruction")
 exec args (Bool x:xs) (JumpIfFalse num:insts) 
     | num < 1 = Left "Invalid number of instructions"
     | num > length insts = Left "Cannot jump this amount of instructions"
@@ -50,10 +53,14 @@ exec args (Bool x:xs) (JumpIfFalse num:insts)
     | otherwise = exec args xs insts
 exec _ _ (JumpIfFalse _:_) = Left "Wrong data types in stack: JumpIfFalse needs a Bool"
 
+execCall :: [Value] -> Either String [Value]
+execCall [] = Left "Stack is empty for Call instruction"
+execCall (Symbol sym:xs) = execBuiltin xs sym
+execCall _ = Left "Stack argument is not a symbol"
 
-execCall :: [Value] -> Ops -> Either String [Value]
-execCall (Number 0:Number 0:_) Div = Left "Cannot divide by 0"
-execCall (Number l:Number r:xs) op = case op of 
+execBuiltin :: [Value] -> Call -> Either String [Value]
+execBuiltin (Number 0:Number 0:_) Div = Left "Cannot divide by 0"
+execBuiltin (Number l:Number r:xs) op = case op of 
     Add -> Right (Number (l + r):xs)
     Sub -> Right (Number (l - r):xs)
     Mul -> Right (Number (l * r):xs)
@@ -65,8 +72,9 @@ execCall (Number l:Number r:xs) op = case op of
     Greater -> Right (Bool (l > r):xs)
     GreaterOrEqual -> Right (Bool (l >= r):xs)
     _ -> Left ("Wrong data types in stack: " ++ show op ++ " with 2 numbers")
-execCall (Bool l:Bool r:xs) op = case op of
+execBuiltin (Bool l:Bool r:xs) op = case op of
     Eq -> Right (Bool (l == r):xs)
     Neq -> Right (Bool (l /= r):xs)
     _ -> Left ("Wrong data types in stack: " ++ show op ++ " with 2 booleans")
-execCall _ _ = Left "Unknown operation"
+execBuiltin _ (Function fct) = Left ("Executing function " ++ fct)
+execBuiltin _ op = Left ("Wrong stack variables for builtin " ++ show op)
