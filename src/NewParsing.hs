c090b9d02f40db663f833ba81e8fc952bfe92561
pablal levy <pablo.levy@epitech.eu>
2024-01-04 00:44:39 +0100

    fix: remove <|> operator since it's either

--- src/NewParsing.hs
+++ src/NewParsing.hs
@@ -2,31 +2,39 @@
 
 module NewParsing (parseDreamberd) where
 
-import NewTypes (AstNode (Number, Operator, String))
+import NewTypes (AstNode (Number, Operator, String, Boolean))
 
-import Data.List (stripPrefix)
+import Data.List (stripPrefix, isPrefixOf)
 import Parsing (Parser(parse))
+import Control.Applicative
+import Data.Char
+import Data.Either
+import Control.Applicative (Alternative (..))
+
 
 data DreamberdType = Number Int
                    | Bool Bool
                    | String String
                    | Function ([DreamberdType] -> DreamberdType)
 
+
 detectType :: DreamberdType -> String
-detectType (NewTypes.Number _) = "Number"
+detectType (NewParsing.Number _) = "Number"
+detectType (NewParsing.String _) = "String"
 detectType (Bool _) = "Bool"
-detectType (NewTypes.String _) = "String"
 detectType (Function _) = "Function"
+
 
 parseNumber :: String -> [AstNode] -> Either String (String, [AstNode])
 parseNumber input ast = case span isDigit input of
     ("", _) -> Left "No number found"
-    (numStr, rest) -> Right (rest, ast ++ [Number (read numStr)])
+    (numStr, rest) -> Right (rest, ast ++ [NewTypes.Number (read numStr)])
+
 
 parseBool :: String -> [AstNode] -> Either String (String, [AstNode])
 parseBool input ast
-    | "true" `isPrefixOf` input  = Right (drop 4 input, ast ++ [Bool True])
-    | "false" `isPrefixOf` input = Right (drop 5 input, ast ++ [Bool False])
+    | "true" `isPrefixOf` input  = Right (drop 4 input, ast ++ [Boolean True])
+    | "false" `isPrefixOf` input = Right (drop 5 input, ast ++ [Boolean False])
     | otherwise                  = Left "No boolean found" 
 
 parseString :: String -> [AstNode] -> Either String (String, [AstNode])
@@ -34,15 +42,20 @@ parseString input ast
     | head input /= '"' = Left "String does not start with a quote"
     | otherwise = case span (/= '"') (tail input) of
         (_, "") -> Left "No closing quote found"
-        (str, rest) -> Right (tail rest, ast ++ [String str])
+        (str, rest) -> Right (tail rest, ast ++ [NewTypes.String str])
+
 
 
 parseVar :: String -> [AstNode] -> Either String (String, [AstNode])
-parseVar input ast = 
-    parseNumber input ast <|> 
-    parseBool input ast <|> 
-    parseString input ast <|>
-    Left "Unrecognized variable type"
+parseVar input ast =
+    case words input of
+        ("int":varName:"=":rest) -> 
+            let (value, remainingCode) = break (== ';') (unwords rest)
+            in case parseNumber (value ++ ";") ast of
+                Right (_, ast') -> Right (dropWhile (== ';') (tail remainingCode), ast' ++ [NewTypes.Number (read varName)])
+                Left err -> Left err
+        _ -> Left "Invalid variable declaration"
+
 
 
 parseElement :: String -> [AstNode] -> Either String (String, [AstNode])
@@ -59,8 +72,3 @@ parseDreamberd sourceCode ast =
     case parseElement sourceCode ast of
         Right (remainingCode, updatedAst) -> parseDreamberd remainingCode updatedAst
         Left err -> Left err
-
-
--- TODO: add condition to stop when no more source code
--- donc le premier call a parseDreamberd sera avec tout le source code et une liste vide, il va se call lui même récursivement
--- et à la fin il va se stop quand y'a plus de source code (ou erreur, à gérer) et renvoyer l'AST complet
