3c620649e204ab5011ecd2281f7c020ad418748e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-03 01:26:17 +0100

    feat(parsing): Add identifier type

--- src/NewTypes.hs
+++ src/NewTypes.hs
@@ -4,6 +4,7 @@ data AstNode
     = Number Integer
     | Boolean Bool
     | String String
+    | Identifier String
     | Function String [String] [AstNode]
     | Call String [AstNode]
     | Operator String AstNode AstNode
@@ -15,6 +16,7 @@ instance Show AstNode where
     show (Boolean True) = "true"
     show (Boolean False) = "false"
     show (String string) = show string
+    show (Identifier identifier) = "id:" ++ show identifier
     show (Operator _symbol _left _right) = "operator"
     show (If _test _trueBody _falseBody) = "if"
     show (List []) = "()"
