7012e4f75293eb8e004dfb965d5ce81cbc3a5050
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-03 13:53:52 +0100

    feat(parsing): New AST types show for condition, function and operators

--- src/NewTypes.hs
+++ src/NewTypes.hs
@@ -18,10 +18,10 @@ instance Show AstNode where
     show (Boolean False) = "false"
     show (String string) = show string
     show (Identifier identifier) = "id:" ++ show identifier
-    show (Operator _symbol _left _right) = "operator"
-    show (If _test _trueBody _falseBody) = "if"
+    show (Operator symbol leftElem rightElem) = "(" ++ show leftElem ++ show symbol ++ show rightElem ++ ")"
+    show (If test trueBody falseBody) = "if (" ++ show test ++ ") {" ++ show trueBody ++ "} else {" ++ show falseBody ++ "}"
     show (List []) = "()"
-    show (List list) = "(" ++ unwords (map show list) ++ ")"
-    show (Function name params body) = "function " ++ show name ++ "(" ++ unwords (map show params) ++ ") {" ++ show body
-    show (Call _name _params) = "call"
+    show (List list) = "[" ++ unwords (map show list) ++ "]"
+    show (Function name params body) = "function " ++ show name ++ "(" ++ unwords (map show params) ++ ") {" ++ show body ++ "}"
+    show (Call name params) = show name ++ "(" ++ show params ++ ")"
     show (Return element) = show element
