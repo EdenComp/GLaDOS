f3be2baeb0128bf5c3987659fea1d83616b120bf
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-04 01:43:55 +0100

    fix(ast): Show functions to display more readable AST

--- src/NewTypes.hs
+++ src/NewTypes.hs
@@ -17,8 +17,8 @@ instance Show AstNode where
     show (Boolean True) = "true"
     show (Boolean False) = "false"
     show (String string) = show string
-    show (Identifier identifier) = "id:" ++ show identifier
-    show (Operator symbol leftElem rightElem) = "(" ++ show leftElem ++ show symbol ++ show rightElem ++ ")"
+    show (Identifier identifier) = "Identifier " ++ identifier
+    show (Operator symbol leftElem rightElem) = "(Operator " ++ show symbol ++ " " ++ show leftElem ++ " " ++ show rightElem ++ ")"
     show (If test trueBody falseBody) = "if (" ++ show test ++ ") {" ++ show trueBody ++ "} else {" ++ show falseBody ++ "}"
     show (List []) = "()"
     show (List list) = "[" ++ unwords (map show list) ++ "]"
