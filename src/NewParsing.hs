86232bcfebcf74ba3bc128474a045be007858d36
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-04 23:55:51 +0100

    feat(parsing/functions): Functional functions parsing

--- src/NewParsing.hs
+++ src/NewParsing.hs
@@ -2,10 +2,9 @@
 
 module NewParsing (parseDreamberd) where
 
-import NewTypes (AstNode (Boolean, Number, Operator, String))
-
 import Data.Char (isDigit, isSpace)
 import Data.List (isPrefixOf, stripPrefix)
+import NewTypes (AstNode (Boolean, Function, Number, Operator, String))
 import Text.Regex.Posix ((=~))
 
 isNotSemiColon :: Char -> Bool
@@ -16,6 +15,28 @@ extractQuotedStringAndRest str = (head matches, rest)
   where
     (before, match, _, matches) = str =~ "(\"[^\"]*\")" :: (String, String, String, [String])
     rest = drop (length before + length match) str
+
+extractFunctionBodyAndRest :: String -> Int -> String -> (String, String)
+extractFunctionBodyAndRest [] _ body = (body, [])
+extractFunctionBodyAndRest (x : xs) openBraces body
+    | x == '{' = extractFunctionBodyAndRest xs (openBraces + 1) (body ++ [x])
+    | x == '}' =
+        if openBraces - 1 == 0
+            then (body, dropWhile isSpace xs)
+            else extractFunctionBodyAndRest xs (openBraces - 1) (body ++ [x])
+    | otherwise = extractFunctionBodyAndRest xs openBraces (body ++ [x])
+
+extractFunctionParts :: String -> (String, [String], String, String)
+extractFunctionParts str = (name, params, body, restOfCode)
+  where
+    (beforeBrace, afterBrace) = break (== '{') str
+    (nameAndParams, _) = break (== ')') beforeBrace
+    (name, parameters) = break (== '(') (dropWhile isSpace nameAndParams)
+    params = words $ map (\c -> if c == ',' then ' ' else c) $ tail parameters
+    (body, restOfCode) = extractFunctionBodyAndRest (drop 1 afterBrace) 1 []
+
+removeSemi :: String -> String
+removeSemi xs = [x | x <- xs, x `notElem` ";"]
 
 parseBool :: String -> Either String AstNode
 parseBool input
@@ -34,9 +55,6 @@ parseString input
     | otherwise = case span (/= '"') (tail input) of
         (_, "") -> Left "No closing quote found for String"
         (str, _) -> Right (String str)
-
-removeSemi :: String -> String
-removeSemi xs = [x | x <- xs, x `notElem` ";"]
 
 parseVar :: String -> String -> [AstNode] -> Either String (String, [AstNode])
 parseVar "int" code ast = case parseNumber value of
@@ -64,6 +82,13 @@ parseVar "str" code ast = case parseString value of
     (value, dropWhile isSpace . drop 1 -> restOfCode) = extractQuotedStringAndRest code
 parseVar _ _ _ = Left "Unrecognized variable type"
 
+parseFunction :: String -> [AstNode] -> Either String (String, [AstNode])
+parseFunction code ast =
+    let (name, params, body, restOfCode) = extractFunctionParts code
+     in case parseDreamberd body [] of
+            Right outputAst -> Right (restOfCode, ast ++ [Function name params outputAst])
+            Left err -> Left err
+
 parseElement :: String -> [AstNode] -> Either String (String, [AstNode])
 parseElement (stripPrefix "int" -> Just restCode) ast =
     case parseVar "int" restCode ast of
@@ -77,12 +102,16 @@ parseElement (stripPrefix "str" -> Just restCode) ast =
     case parseVar "str" restCode ast of
         Right (remainingCode, updatedAst) -> Right (remainingCode, updatedAst)
         Left err -> Left err
+parseElement (stripPrefix "function" -> Just restCode) ast =
+    case parseFunction restCode ast of
+        Right (remainingCode, updatedAst) -> Right (remainingCode, updatedAst)
+        Left err -> Left err
 parseElement _ _ = Left "Unrecognized element"
 
 parseDreamberd :: String -> [AstNode] -> Either String [AstNode]
 parseDreamberd "\n" ast = Right ast
 parseDreamberd "" ast = Right ast
 parseDreamberd sourceCode ast =
-    case parseElement sourceCode ast of
+    case parseElement (dropWhile isSpace sourceCode) ast of
         Right (remainingCode, updatedAst) -> parseDreamberd remainingCode updatedAst
         Left err -> Left err
