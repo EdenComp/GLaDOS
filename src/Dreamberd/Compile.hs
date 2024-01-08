1ef5d439dc97acf95556247e3a73667a4a9fd05b
Croos3r <dorian.moy@epitech.eu>
2024-01-09 00:28:01 +0100

    feat(cumpilation💦): made basic compilation of operation and assignations

--- src/Dreamberd/Compile.hs
+++ src/Dreamberd/Compile.hs
@@ -1,9 +1,63 @@
 module Dreamberd.Compile (
     compileAst,
+    compileExecAst,
 ) where
 
-import Dreamberd.Types (AstNode (..))
-import Dreamberd.Vm (Insts (..), Value (..))
+import Control.Monad ((>=>))
+import qualified Dreamberd.Types as AST
+import qualified Dreamberd.Vm as VM
 
-compileAst :: [AstNode] -> Either String [Insts]
-compileAst _ = Right [Push (Dreamberd.Vm.String "cramptarea will be back"), Ret]
+compileExecAst :: [AST.AstNode] -> IO (Either String VM.Value)
+compileExecAst ast = either (pure . Left) (VM.exec [] [] []) (compileAst ast)
+
+compileAst :: [AST.AstNode] -> Either String [VM.Insts]
+compileAst [] = Left "No instructions provided"
+compileAst ast = compileNodes ast []
+
+compileNodes :: [AST.AstNode] -> [VM.Insts] -> Either String [VM.Insts]
+compileNodes [] insts = Right insts
+compileNodes (curNode : nextNodes) insts = compileNode curNode >>= \curInsts -> compileNodes nextNodes (insts ++ curInsts)
+
+compileNode :: AST.AstNode -> Either String [VM.Insts]
+compileNode (AST.Call op args) = compileCall op args
+compileNode (AST.Return value) = compileValue value >>= \r -> Right [VM.Push r, VM.Ret]
+compileNode _ = Left "Unknown node type"
+
+compileCall :: String -> [AST.AstNode] -> Either String [VM.Insts]
+compileCall "=" [AST.Identifier iden, value] = compileAssignation iden value
+compileCall op args = compileBuiltinCall op args <> compileCustomCall op args
+
+compileBuiltinCall :: String -> [AST.AstNode] -> Either String [VM.Insts]
+compileBuiltinCall op [a, b] =
+    getBuiltinCallForOp op
+        >>= \call ->
+            compileValue a
+                >>= \a' ->
+                    compileValue b
+                        >>= \b' -> Right [VM.Push b', VM.Push a', VM.Push $ VM.Symbol call, VM.Call]
+compileBuiltinCall _ _ = Left "Unknown builtin call"
+
+getBuiltinCallForOp :: String -> Either String VM.Call
+getBuiltinCallForOp "+" = Right VM.Add
+getBuiltinCallForOp "-" = Right VM.Sub
+getBuiltinCallForOp "*" = Right VM.Mul
+getBuiltinCallForOp "/" = Right VM.Div
+getBuiltinCallForOp "==" = Right VM.Eq
+getBuiltinCallForOp "!=" = Right VM.Neq
+getBuiltinCallForOp "<" = Right VM.Less
+getBuiltinCallForOp "<=" = Right VM.LessOrEqual
+getBuiltinCallForOp ">" = Right VM.Greater
+getBuiltinCallForOp ">=" = Right VM.GreaterOrEqual
+getBuiltinCallForOp _ = Left "Unknown builtin call"
+
+compileCustomCall :: String -> [AST.AstNode] -> Either String [VM.Insts]
+compileCustomCall name args = mapM (compileValue >=> (Right . VM.Push)) args >>= \args' -> Right $ reverse args' ++ [VM.PushEnv name, VM.Call]
+
+compileAssignation :: String -> AST.AstNode -> Either String [VM.Insts]
+compileAssignation iden v = compileValue v >>= \r -> Right [VM.DefineEnv iden $ VM.Variable r]
+
+compileValue :: AST.AstNode -> Either String VM.Value
+compileValue (AST.Number val) = Right $ VM.Number val
+compileValue (AST.Boolean val) = Right $ VM.Bool val
+compileValue (AST.String val) = Right $ VM.String val
+compileValue _ = Left "Unknown value type"
--- src/Dreamberd/Types.hs
+++ src/Dreamberd/Types.hs
@@ -11,7 +11,6 @@ data AstNode
     | Return AstNode
     | Loop AstNode [AstNode] (Maybe AstNode) (Maybe AstNode)
     | List [AstNode]
-    deriving (Eq)
 
 instance Show AstNode where
     show (Number val) = show val
@@ -25,3 +24,59 @@ instance Show AstNode where
     show (Call name params) = "(Call " ++ name ++ " " ++ show params ++ ")"
     show (Loop test body initializer update) = "(Loop " ++ show test ++ " " ++ show body ++ " " ++ show initializer ++ " " ++ show update ++ ")"
     show (Return element) = "Return " ++ show element
+
+instance Eq AstNode where
+    (Number a) == (Number b) = a == b
+    (Boolean a) == (Boolean b) = a == b
+    (String a) == (String b) = a == b
+    (Identifier a) == (Identifier b) = a == b
+    (Operator a b c) == (Operator d e f) = a == d && b == e && c == f
+    (If a b c) == (If d e f) = a == d && b == e && c == f
+    (List a) == (List b) = a == b
+    (Function a b c) == (Function d e f) = a == d && b == e && c == f
+    (Call a b) == (Call d e) = a == d && b == e
+    (Loop a b c d) == (Loop e f g h) = a == e && b == f && c == g && d == h
+    (Return a) == (Return b) = a == b
+    _ == _ = False
+
+instance Ord AstNode where
+    (Number a) `compare` (Number b) = a `compare` b
+    (Boolean a) `compare` (Boolean b) = a `compare` b
+    (String a) `compare` (String b) = a `compare` b
+    (Identifier a) `compare` (Identifier b) = a `compare` b
+    (Operator a b c) `compare` (Operator d e f) = a `compare` d <> b `compare` e <> c `compare` f
+    (If a b c) `compare` (If d e f) = a `compare` d <> b `compare` e <> c `compare` f
+    (List a) `compare` (List b) = a `compare` b
+    (Function a b c) `compare` (Function d e f) = a `compare` d <> b `compare` e <> c `compare` f
+    (Call a b) `compare` (Call d e) = a `compare` d <> b `compare` e
+    (Loop a b c d) `compare` (Loop e f g h) = a `compare` e <> b `compare` f <> c `compare` g <> d `compare` h
+    (Return a) `compare` (Return b) = a `compare` b
+    _ `compare` _ = EQ
+
+instance Num AstNode where
+    (Number a) + (Number b) = Number (a + b)
+    _ + _ = error "Cannot add non-number values"
+    (Number a) - (Number b) = Number (a - b)
+    _ - _ = error "Cannot subtract non-number values"
+    (Number a) * (Number b) = Number (a * b)
+    _ * _ = error "Cannot multiply non-number values"
+    abs (Number a) = Number (abs a)
+    abs _ = error "Cannot get absolute value of non-number value"
+    signum (Number a) = Number (signum a)
+    signum _ = error "Cannot get signum of non-number value"
+    fromInteger a = Number (fromInteger a)
+
+instance Integral AstNode where
+    toInteger (Number a) = toInteger a
+    toInteger _ = error "Cannot convert non-number value to integer"
+    quotRem (Number a) (Number b) = (Number (quot a b), Number (rem a b))
+    quotRem _ _ = error "Cannot divide non-number values"
+
+instance Real AstNode where
+    toRational (Number a) = toRational a
+    toRational _ = error "Cannot convert non-number value to rational"
+
+instance Enum AstNode where
+    toEnum a = Number (toEnum a)
+    fromEnum (Number a) = fromEnum a
+    fromEnum _ = error "Cannot convert non-number value from enum"
