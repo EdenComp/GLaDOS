1b0641fd6553e71da2189b931a075f5551f77290
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-08 13:37:23 +0100

    fix(parsing/var): Check if operator is equal sign

--- src/Dreamberd/Parsing/Elements/Variable.hs
+++ src/Dreamberd/Parsing/Elements/Variable.hs
@@ -6,12 +6,17 @@ import Dreamberd.Types (AstNode (Operator, String))
 
 parseVar :: String -> String -> [AstNode] -> Either String (String, [AstNode])
 parseVar _ code ast =
-    case getVariableName (head (words code)) of
-        Right name ->
-            let
-                (value, restOfCode) = extractValueAndRest (unwords (drop 2 (words code)))
-             in
-                case parseAnyValue value of
-                    Right node -> Right (restOfCode, ast ++ [Operator "=" (String name) node])
-                    Left err -> Left err
-        Left err -> Left err
+    let
+        wordsInCode = words code
+     in
+        if length wordsInCode < 2 || (wordsInCode !! 1) /= "="
+            then Left "Expected '=' after variable name"
+            else case getVariableName (head wordsInCode) of
+                Right name ->
+                    let
+                        (value, restOfCode) = extractValueAndRest (unwords (drop 2 wordsInCode))
+                     in
+                        case parseAnyValue value of
+                            Right node -> Right (restOfCode, ast ++ [Operator "=" (String name) node])
+                            Left err -> Left err
+                Left err -> Left err
