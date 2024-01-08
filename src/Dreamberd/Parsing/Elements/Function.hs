4a2df551aee8e3b9c98dd9d6fee94197979d5d8b
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-08 16:15:38 +0100

    fix(parsing/functions): Parenthesis in params

--- src/Dreamberd/Parsing/Elements/Function.hs
+++ src/Dreamberd/Parsing/Elements/Function.hs
@@ -7,15 +7,18 @@ import Dreamberd.Types (AstNode (Return))
 
 extractFunctionParts :: String -> Either String (String, [String], String, String)
 extractFunctionParts str =
-    let (nameAndParams, restAfterParams) = break (== ')') str
-        (_, parameters) = break (== '(') (dropWhile isSpace nameAndParams)
-        params = words $ map (\c -> if c == ',' then ' ' else c) $ tail parameters
-     in case getVariableName (dropWhile isSpace nameAndParams) of
-            Left err -> Left err
-            Right name ->
-                case parseScope (drop 1 restAfterParams) of
+    if not ('(' `elem` str && ')' `elem` str)
+        then Left "No parenthesis found for function params"
+        else
+            let (nameAndParams, restAfterParams) = break (== ')') str
+                (_, parameters) = break (== '(') (dropWhile isSpace nameAndParams)
+                params = words $ map (\c -> if c == ',' then ' ' else c) $ tail parameters
+             in case getVariableName (dropWhile isSpace nameAndParams) of
                     Left err -> Left err
-                    Right (body, restOfCode) -> Right (name, params, body, restOfCode)
+                    Right name ->
+                        case parseScope (drop 1 restAfterParams) of
+                            Left err -> Left err
+                            Right (body, restOfCode) -> Right (name, params, body, restOfCode)
 
 parseReturn :: String -> [AstNode] -> Either String (String, [AstNode])
 parseReturn code ast =
