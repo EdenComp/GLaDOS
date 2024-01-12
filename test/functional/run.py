f10cc9dd7ae2f056e0535640c93ca27fc92e0869
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-01-12 08:32:32 +0100

    feat(func_test): adapt run.py to test compile + exec on dreamberd code

--- test/functional/run.py
+++ test/functional/run.py
@@ -7,15 +7,15 @@ from difflib import unified_diff as diff
 from glob import glob
 import re
 
-LANGUAGES = [("Lisp", ".lsp", "lisp", "lisp"), ("Dreamberd", ".db4", "dreamberd", "-c")]
+LANGUAGES = [("Lisp", ".lsp", "lisp", "lisp", ""), ("Dreamberd", ".db4", "dreamberd", "compile", ";./glados execute")]
 
 def get_test_paths(folder_name: str, extension: str) -> list[str]:
     folder = path.dirname(path.realpath(__file__))
     return glob(folder + f'/src/{folder_name}/*{extension}', recursive=True)
 
 
-def run_command(test_path: str, binary: str) -> CompletedProcess[str]:
-    return run(f'{binary} {test_path}', shell=True, capture_output=True, text=True)
+def run_command(test_path: str, binary: str, after_command: str) -> CompletedProcess[str]:
+    return run(f'{binary} {test_path} {after_command}', shell=True, capture_output=True, text=True)
 
 
 def disp_err(output: CompletedProcess[str], expected_output: str, expected_error: str) -> None:
@@ -32,9 +32,40 @@ def disp_err(output: CompletedProcess[str], expected_output: str, expected_error
             print(line, end="")
         print()
 
+#!/usr/bin/env python
+from subprocess import CompletedProcess, run
+from sys import argv
+from termcolor import colored
+from os import path
+from difflib import unified_diff as diff
+from glob import glob
+import re
 
-def run_test(test_path: str, extension: str, flag: str, is_debug: bool, is_full_log: bool, has_color: bool) -> bool:
-    output = run_command(test_path, f"./glados {flag}")
+LANGUAGES = [("Lisp", ".lsp", "lisp", "lisp", ""), ("Dreamberd", ".db4", "dreamberd", "compile", ";./glados execute a.out")]
+
+def get_test_paths(folder_name: str, extension: str) -> list[str]:
+    folder = path.dirname(path.realpath(__file__))
+    return glob(folder + f'/src/{folder_name}/*{extension}', recursive=True)
+
+def run_command(test_path: str, binary: str, after_command: str) -> CompletedProcess[str]:
+    return run(f'{binary} {test_path} {after_command}', shell=True, capture_output=True, text=True)
+
+def disp_err(output: CompletedProcess[str], expected_output: str, expected_error: str) -> None:
+    if output.stdout != expected_output:
+        print("stdout:")
+        delta = diff(output.stdout.split(), expected_output.split(), "glados", "test")
+        for line in delta:
+            print(line, end="")
+        print()
+    if output.stderr != expected_error:
+        print("stderr:")
+        delta = diff(output.stderr.split(), expected_error.split(), "glados", "test")
+        for line in delta:
+            print(line, end="")
+        print()
+
+def run_test(test_path: str, extension: str, flag: str, after_command: str, is_debug: bool, is_full_log: bool, has_color: bool) -> bool:
+    output = run_command(test_path, f"./glados {flag}", after_command)
     expected_output, expected_error = '', ''
 
     if path.exists(test_path.replace(extension, ".out")):
@@ -53,6 +84,7 @@ def run_test(test_path: str, extension: str, flag: str, is_debug: bool, is_full_
         err = f'Test "{name}" failed.'
         print(colored(err, "red") if has_color else err)
         return False
+
     if is_full_log:
         success = f'Test "{name}" passed.'
         print(colored(success, "green") if has_color else success)
@@ -66,18 +98,18 @@ if __name__ == "__main__":
 
     total_tests, total_passed = 0, 0
 
-    for name, extension, folder, flag in LANGUAGES:
-        print(f"Running {name} tests:")
-        nb_passed, nb_failed = 0, 0
-        tests = get_test_paths(folder_name=folder, extension=extension)
-        nb_tests = len(tests)
+    for name, extension, folder, flag, after_command in LANGUAGES:
+         print(f"Running {name} tests:")
+         nb_passed, nb_failed = 0, 0
+         tests = get_test_paths(folder_name=folder, extension=extension)
+         nb_tests = len(tests)
 
-        for test in tests:
-            passed = run_test(test, extension, flag, is_debug, is_full_log, has_color)
-            nb_passed += 1 if passed else 0
-            nb_failed += 1 if not passed else 0
-        print(f"Ran {nb_tests} tests ({nb_passed} passed and {nb_failed} failed).\n")
-        total_tests += nb_tests
-        total_passed += nb_passed
+         for test in tests:
+             passed = run_test(test, extension, flag, after_command, is_debug, is_full_log, has_color)
+             nb_passed += 1 if passed else 0
+             nb_failed += 1 if not passed else 0
+         print(f"Ran {nb_tests} tests ({nb_passed} passed and {nb_failed} failed).\n")
+         total_tests += nb_tests
+         total_passed += nb_passed
 
     exit(nb_tests - nb_passed)
