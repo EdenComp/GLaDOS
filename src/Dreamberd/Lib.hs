78ecb19aa5ad9527d0ec841cdefbfa958ffef296
Florian Lauch <florian.lauchh@gmail.com>
2024-01-13 00:53:07 +0100

    feat: Preprocessing (1): Imports

--- src/Dreamberd/Lib.hs
+++ src/Dreamberd/Lib.hs
@@ -18,6 +18,22 @@ import Dreamberd.Types (File (..))
 import Dreamberd.Vm (Insts (..), Value (..), execVM)
 import System.Exit (ExitCode (ExitFailure), exitWith)
 
+{--
+import Dreamberd.Compilation.Compile (compileAst)
+import Dreamberd.Parsing.Main (parseDreamberd)
+import Dreamberd.Compilation.Preprocessing (executePreprocessing)
+import Dreamberd.Vm (Insts (..), execVM)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+
+compileDreamberdCode :: String -> String -> IO ()
+compileDreamberdCode sourceCode outputFile =
+    case parseDreamberd sourceCode [] of
+        Right ast -> executePreprocessing ast >>= \case
+            Right ast' -> case compileAst ast' of
+                Right insts -> writeFile outputFile (transpileIntoBytecode insts)
+                Left err -> returnWithError err
+--}
+
 compileDreamberdCode :: File String -> String -> IO ()
 compileDreamberdCode (File filename sourcecode) outputFile =
     case parseDreamberd (File filename sourcecode) of
@@ -46,6 +62,16 @@ executeDreamberdInsts insts = do
         Right val -> putStrLn ("Warning: main scope returned a non-numerical value: " ++ show val)
         Left err -> returnWithError err
 
+{--
+runDreamberdCode :: String -> IO ()
+runDreamberdCode sourceCode =
+    case parseDreamberd sourceCode [] of
+        Right ast -> executePreprocessing ast >>= \case
+            Right ast' -> case compileAst ast' of
+                Right insts -> executeDreamberdInsts insts
+                Left err -> returnWithError err
+--}
+
 runDreamberdCode :: File String -> IO ()
 runDreamberdCode (File filename sourcecode) =
     case parseDreamberd (File filename sourcecode) of
@@ -61,6 +87,14 @@ compileToAst :: File String -> IO ()
 compileToAst file =
     case parseDreamberd file of
         Right ast -> print ast
+        Left err -> returnWithError err
+        
+compileToPreprocessedAst :: String -> IO ()
+compileToPreprocessedAst sourceCode =
+    case parseDreamberd sourceCode [] of
+        Right ast -> executePreprocessing ast >>= \case
+            Right ast' -> print ast'
+            Left err -> returnWithError err
         Left err -> returnWithError err
 
 compileToPreprocessedAst :: File String -> IO ()
