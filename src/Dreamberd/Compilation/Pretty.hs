bdf2279e6c7b14d9b26eafa5e3bb5360ed0c092e
Florian Lauch <florian.lauchh@gmail.com>
2024-01-14 08:38:18 +0100

    fix: linter

--- src/Dreamberd/Compilation/Pretty.hs
+++ src/Dreamberd/Compilation/Pretty.hs
@@ -2,9 +2,9 @@ module Dreamberd.Compilation.Pretty (
     prettyPrintAST,
 ) where
 
-import Dreamberd.Types (AstNode (..))
 import Data.List (intercalate)
 import Data.Maybe (maybeToList)
+import Dreamberd.Types (AstNode (..))
 
 prettyPrintAST :: [AstNode] -> String
 prettyPrintAST insts = prettyPrintNodes insts 0
@@ -16,7 +16,7 @@ prettyPrintNodes (Float nbr : xs) sp = replicate sp ' ' ++ "Float " ++ show nbr 
 prettyPrintNodes (Boolean b : xs) sp = replicate sp ' ' ++ "Bool " ++ show b ++ "\n" ++ prettyPrintNodes xs sp
 prettyPrintNodes (String str : xs) sp = replicate sp ' ' ++ "String " ++ show str ++ "\n" ++ prettyPrintNodes xs sp
 prettyPrintNodes (Identifier sym : xs) sp = replicate sp ' ' ++ "Identifier " ++ show sym ++ "\n" ++ prettyPrintNodes xs sp
-prettyPrintNodes (Function name params body : xs) sp = replicate sp ' ' ++ "Function " ++ name ++ " (" ++ intercalate ", " params ++ ") {\n" ++ prettyPrintNodes body (sp + 2) ++ replicate sp ' '  ++ "}\n" ++ prettyPrintNodes xs sp
+prettyPrintNodes (Function name params body : xs) sp = replicate sp ' ' ++ "Function " ++ name ++ " (" ++ intercalate ", " params ++ ") {\n" ++ prettyPrintNodes body (sp + 2) ++ replicate sp ' ' ++ "}\n" ++ prettyPrintNodes xs sp
 prettyPrintNodes (Call name params : xs) sp = replicate sp ' ' ++ "Call " ++ name ++ " (\n" ++ prettyPrintNodes params (sp + 2) ++ replicate sp ' ' ++ ")\n" ++ prettyPrintNodes xs sp
 prettyPrintNodes (If cond trueBody falseBody : xs) sp = replicate sp ' ' ++ "If (\n" ++ prettyPrintNodes [cond] (sp + 2) ++ replicate sp ' ' ++ ") {\n" ++ prettyPrintNodes trueBody (sp + 2) ++ replicate sp ' ' ++ "} else {\n" ++ prettyPrintNodes falseBody (sp + 2) ++ replicate sp ' ' ++ "}\n" ++ prettyPrintNodes xs sp
 prettyPrintNodes (Return val : xs) sp = replicate sp ' ' ++ "Return (\n" ++ prettyPrintNodes (maybeToList val) (sp + 2) ++ replicate sp ' ' ++ ")\n" ++ prettyPrintNodes xs sp
