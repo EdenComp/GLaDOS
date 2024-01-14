c341336e66c8b16b602c9f58e93fc2515005ccee
Florian Lauch <florian.lauchh@gmail.com>
2024-01-14 02:36:56 +0100

    fix: compilation and unit tests

--- test/Unit/Dreamberd/TestDreamberdBytecode.hs
+++ test/Unit/Dreamberd/TestDreamberdBytecode.hs
@@ -6,8 +6,8 @@ import Dreamberd.Vm (
     EnvValue (..),
     Insts (..),
     Operator (..),
+    Value (..),
  )
-import qualified Dreamberd.Vm as VM
 import Test.HUnit (Test (..), assertEqual)
 
 testDreamberdBytecode :: Test
@@ -23,13 +23,13 @@ testTypesTranspilation =
         [ TestCase $ assertEqual "Transpile Int" (transpileInt 1) "\x00\x00\x00\x00\x00\x00\x00\x01"
         , TestCase $ assertEqual "Transpile Int Zero" (transpileInt 0) "\x00\x00\x00\x00\x00\x00\x00\x00"
         , TestCase $ assertEqual "Transpile Int Negative" (transpileInt (-1)) "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"
-        , TestCase $ assertEqual "Transpile Float" (transpileInstruction (Push (VM.Float 1.0))) "\SOH\DC2Q\NUL\NUL\NUL\NUL\NUL\NUL\NUL\a\DLE\NUL\NUL\NUL\NUL\NUL\NUL\255\255\255\255\255\255\255\204"
+        , TestCase $ assertEqual "Transpile Float" (transpileInstruction (Push (Float 1.0))) "\SOH\DC2Q\NUL\NUL\NUL\NUL\NUL\NUL\NUL\a\DLE\NUL\NUL\NUL\NUL\NUL\NUL\255\255\255\255\255\255\255\204"
         , TestCase $ assertEqual "Transpile String" (transpileString "Hello") "\x00\x00\x00\x00\x00\x00\x00\x05Hello"
         , TestCase $ assertEqual "Transpile String 2" (transpileString "World") "\x00\x00\x00\x00\x00\x00\x00\x05World"
         , TestCase $ assertEqual "Transpile Empty String" (transpileString "") "\x00\x00\x00\x00\x00\x00\x00\x00"
         , TestCase $ assertEqual "Transpile Call Add" (transpileCall (Builtin Add)) "\x21\x31"
         , TestCase $ assertEqual "Transpile Call FunctionName" (transpileCall (FunctionName "test")) "\x22\x00\x00\x00\x00\x00\x00\x00\x04test"
-        , TestCase $ assertEqual "Transpile Instruction Push" (transpileInstruction (Push (VM.Integer 1))) "\x01\x11\x00\x00\x00\x00\x00\x00\x00\x01"
+        , TestCase $ assertEqual "Transpile Instruction Push" (transpileInstruction (Push (Integer 1))) "\x01\x11\x00\x00\x00\x00\x00\x00\x00\x01"
         , TestCase $ assertEqual "Transpile Instruction PushArg" (transpileInstruction (PushArg 1)) "\x02\x00\x00\x00\x00\x00\x00\x00\x01"
         , TestCase $ assertEqual "Transpile Instruction PushEnv" (transpileInstruction (PushEnv "test")) "\x03\x00\x00\x00\x00\x00\x00\x00\x04test"
         , TestCase $ assertEqual "Transpile Instruction Call" (transpileInstruction Call) "\x04"
