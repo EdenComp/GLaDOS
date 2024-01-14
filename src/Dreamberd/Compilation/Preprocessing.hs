50b52d1f5cb3e087817552414b79c0e558fd33da
Florian Lauch <florian.lauchh@gmail.com>
2024-01-14 14:44:29 +0100

    fix: circular dependency last issues

--- src/Dreamberd/Compilation/Preprocessing.hs
+++ src/Dreamberd/Compilation/Preprocessing.hs
@@ -4,7 +4,7 @@ module Dreamberd.Compilation.Preprocessing (
     executePreprocessing,
 ) where
 
-import Data.List (partition)
+import Data.List (partition, elemIndex, intercalate)
 import Dreamberd.Parser (parseDreamberd)
 import Dreamberd.Types (AstNode (..), File (..))
 import System.Directory (getCurrentDirectory, getHomeDirectory)
@@ -18,7 +18,8 @@ executePreprocessing file =
 preprocessImports :: File [AstNode] -> IO (Either String [AstNode])
 preprocessImports (File filename nodes) = do
     current <- getCurrentDirectory
-    preprocessNodes nodes (if head filename == '/' then filename else current ++ "/" ++ filename) [if head filename == '/' then filename else current ++ "/" ++ filename] >>= \case
+    let path = if head filename == '/' then filename else current ++ "/" ++ filename
+    preprocessNodes nodes path [sanitizePath path] >>= \case
         Left err -> return $ Left err
         Right (_, nodes') -> return $ Right nodes'
 
@@ -65,12 +66,34 @@ resolvePath (x : xs) = x : resolvePath xs
 getFileDirectory :: FilePath -> FilePath
 getFileDirectory = reverse . dropWhile (/= '/') . reverse
 
+sanitizePath :: String -> FilePath
+sanitizePath path = sanitizeDoubleSlashes $ sanitizeDoubleDots $ sanitizeSimpleDots path
+
+sanitizeSimpleDots :: String -> FilePath
+sanitizeSimpleDots [] = []
+sanitizeSimpleDots ['/', '.'] = "/"
+sanitizeSimpleDots ('/' : '.' : '/' : xs) = sanitizeSimpleDots ('/' : xs)
+sanitizeSimpleDots (x : xs) = x : sanitizeSimpleDots xs
+
+sanitizeDoubleDots :: String -> FilePath
+sanitizeDoubleDots [] = []
+sanitizeDoubleDots path = case elemIndex ".." folders of 
+    Just idx -> sanitizeDoubleDots $ '/' : intercalate "/" (take (idx - 1) folders ++ drop (idx + 1) folders)
+    Nothing -> path
+  where
+    folders = words [if c == '/' then ' ' else c | c <- path]
+
+sanitizeDoubleSlashes :: String -> FilePath
+sanitizeDoubleSlashes [] = []
+sanitizeDoubleSlashes ('/' : '/' : xs) = sanitizeDoubleSlashes ('/' : xs)
+sanitizeDoubleSlashes (x : xs) = x : sanitizeDoubleSlashes xs
+
 resolveImport :: FilePath -> [String] -> IO (Maybe ([String], String))
 resolveImport path imports = do
     file <- readFile path
-    if path `elem` imports
+    if sanitizePath path `elem` imports
         then return Nothing
-        else return $ Just (path : imports, file)
+        else return $ Just (sanitizePath path : imports, file)
 
 importFile :: String -> ([String], String) -> IO (Either String ([String], [AstNode]))
 importFile lib (imports, code) = case parseDreamberd (File lib code) of
