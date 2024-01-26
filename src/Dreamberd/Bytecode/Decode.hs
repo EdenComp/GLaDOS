69b4f165530c9b5c492fa26cd876f02227c01d3c
Florian Lauch <florian.lauchh@gmail.com>
2024-01-26 11:46:19 +0100

    feat: reduced size indexes

--- src/Dreamberd/Bytecode/Decode.hs
+++ src/Dreamberd/Bytecode/Decode.hs
@@ -6,6 +6,17 @@ import Data.Bits (shiftL, (.|.))
 import Data.Char (ord)
 import Dreamberd.Vm (Call (..), DefineEnvType (..), Insts (..), Value (..))
 
+parseSize :: [Char] -> Either String (Int, [Char])
+parseSize (size : bytes) | length bytes >= sized = Right (parseUnsignedInt val sized, rest)
+  where
+    sized = fromEnum size
+    (val, rest) = splitAt sized bytes
+parseSize _ = Left "Not enough space for a size"
+
+parseUnsignedInt :: [Char] -> Int -> Int
+parseUnsignedInt [] _ = 0
+parseUnsignedInt (x:xs) idx = max (256 ^ (idx - 1)) 1 * fromEnum x + parseUnsignedInt xs (idx - 1)
+
 parseInt :: [Char] -> Either String (Int, [Char])
 parseInt (b1 : b2 : b3 : b4 : b5 : b6 : b7 : b8 : rest) = Right ((ord b1 `shiftL` 56) .|. (ord b2 `shiftL` 48) .|. (ord b3 `shiftL` 40) .|. (ord b4 `shiftL` 32) .|. (ord b5 `shiftL` 24) .|. (ord b6 `shiftL` 16) .|. (ord b7 `shiftL` 8) .|. ord b8, rest)
 parseInt _ = Left "Not enough space for an int"
@@ -17,13 +28,13 @@ parseInteger' _ _ = Left "Not enough space for an integer"
 
 parseInteger :: [Char] -> Either String (Integer, [Char])
 parseInteger (c : bytes)
-    | fromEnum c == 0x51 = parseInt bytes >>= uncurry parseInteger'
-    | fromEnum c == 0x52 = parseInt bytes >>= \(size, rest) -> parseInteger' size rest >>= \(val, rest') -> Right (-val, rest')
+    | fromEnum c == 0x51 = parseSize bytes >>= uncurry parseInteger'
+    | fromEnum c == 0x52 = parseSize bytes >>= \(size, rest) -> parseInteger' size rest >>= \(val, rest') -> Right (-val, rest')
 parseInteger _ = Left "Not enough space for an integer"
 
 parseString :: [Char] -> Either String (String, [Char])
 parseString bytes =
-    parseInt bytes >>= \(val, rest) ->
+    parseSize bytes >>= \(val, rest) ->
         if length rest < val then Left "Wrong string length" else Right (splitAt val rest)
 
 parseCall :: [Char] -> Either String (Call, [Char])
@@ -47,8 +58,8 @@ parseValue (c : bytes) = case fromEnum c of
     0x14 -> parseString bytes >>= \(val, rest) -> Right (String val, rest)
     0x15 -> parseCall bytes >>= \(val, rest) -> Right (Symbol val, rest)
     0x16 ->
-        parseInt bytes >>= \(args, rest) ->
-            parseInt rest >>= \(len, rest') ->
+        parseSize bytes >>= \(args, rest) ->
+            parseSize rest >>= \(len, rest') ->
                 let (insts, rest'') = splitAt len rest' in parseInstructions insts >>= \func -> if length rest' < len then Left "Wrong lambda body length" else Right (Lambda args func, rest'')
     0x17 -> Right (Void, bytes)
     _ -> Left "Unknown value type"
@@ -80,7 +91,7 @@ parseInstructions :: [Char] -> Either String [Insts]
 parseInstructions [] = Right []
 parseInstructions (c : bytes) = case fromEnum c of
     0x01 -> parseValue bytes >>= \(val, rest) -> pursueParsing (Push val) rest
-    0x02 -> parseInt bytes >>= \(val, rest) -> pursueParsing (PushArg val) rest
+    0x02 -> parseSize bytes >>= \(val, rest) -> pursueParsing (PushArg val) rest
     0x03 -> parseString bytes >>= \(val, rest) -> pursueParsing (PushEnv val) rest
     0x04 -> pursueParsing Call bytes
     0x05 -> parseString bytes >>= uncurry parseDefineEnv >>= uncurry pursueParsing
